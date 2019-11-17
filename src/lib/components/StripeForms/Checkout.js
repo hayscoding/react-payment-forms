24e93fbb698822cbf2c4aaa227c5428daf22b7ef
Hays Stanford <stanfordhays@gmail.com>
2019-11-17 16:02:40 -0600

    code formatting in Checkout.js

--- src/lib/components/StripeForms/Checkout.js
+++ src/lib/components/StripeForms/Checkout.js
@@ -12,16 +12,15 @@ import styles from '../styles/StripeForms.css'
 export default class Checkout extends React.Component {
   constructor() {
     super();
+
     this.state = {
       elementFontSize: window.innerWidth < 450 ? '14px' : '18px',
     };
+
     window.addEventListener('resize', () => {
       if (window.innerWidth < 450 && this.state.elementFontSize !== '14px') {
         this.setState({elementFontSize: '14px'});
-      } else if (
-        window.innerWidth >= 450 &&
-        this.state.elementFontSize !== '18px'
-      ) {
+      } else if (window.innerWidth >= 450 && this.state.elementFontSize !== '18px') {
         this.setState({elementFontSize: '18px'});
       }
     });
