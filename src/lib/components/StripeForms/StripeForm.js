25f6b834b975d14a69449223361496ad524502a5
Hays Stanford <stanfordhays@gmail.com>
2019-11-17 15:46:37 -0600

    removed commented IdealBankForm code from StripeForm.js

--- src/lib/components/StripeForms/StripeForm.js
+++ src/lib/components/StripeForms/StripeForm.js
@@ -181,52 +181,6 @@ class _IbanForm extends React.Component {
 }
 const IbanForm = injectStripe(_IbanForm);
 
-// class _IdealBankForm extends React.Component {
-//   handleSubmit = (ev) => {
-//     ev.preventDefault();
-//     if (this.props.stripe) {
-//       this.props.stripe
-//         .createSource({
-//           type: 'ideal',
-//           amount: 1099,
-//           currency: 'eur',
-//           owner: {
-//             name: ev.target.name.value,
-//           },
-//           redirect: {
-//             return_url: 'https://example.com',
-//           },
-//         })
-//         .then((payload) => console.log('[source]', payload));
-//     } else {
-//       console.log("Stripe.js hasn't loaded yet.");
-//     }
-//   };
-//   render() {
-//     return (
-//       <form onSubmit={this.handleSubmit}>
-//         <label>
-//           Name
-//           <input name="name" type="text" placeholder="Jane Doe" required />
-//         </label>
-//         <label>
-//           iDEAL Bank
-//           <IdealBankElement
-//             className="IdealBankElement"
-//             onBlur={handlers.handleBlur}
-//             onChange={handlers.handleChange}
-//             onFocus={handlers.handleFocus}
-//             onReady={handlers.handleReady}
-//             {...createOptions(this.props.fontSize, '10px 14px')}
-//           />
-//         </label>
-//         <button>Pay</button>
-//       </form>
-//     );
-//   }
-// }
-// const IdealBankForm = injectStripe(_IdealBankForm);
-
 class Checkout extends React.Component {
   constructor() {
     super();
