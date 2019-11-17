d652ef75f6cba7a60a9e9ab5af7c686c32d269f8
Hays Stanford <stanfordhays@gmail.com>
2019-11-17 15:48:00 -0600

    moved code for IbanForm to IbanForm.js

--- src/lib/components/StripeForms/StripeForm.js
+++ src/lib/components/StripeForms/StripeForm.js
@@ -15,6 +15,7 @@ import {
 import CardForm from './CardForm.js'
 import SplitForm from './SplitForm.js'
 import IdealBankForm from './IdealBankForm.js'
+import IbanForm from './IbanForm.js'
 import createOptions from './utils/createOptions.js'
 
 import handlers from './utils/handlers.js'
@@ -125,61 +126,6 @@ class _PaymentRequestForm extends React.Component {
   }
 }
 const PaymentRequestForm = injectStripe(_PaymentRequestForm);
-
-class _IbanForm extends React.Component {
-  handleSubmit = (ev) => {
-    ev.preventDefault();
-    if (this.props.stripe) {
-      this.props.stripe
-        .createSource({
-          type: 'sepa_debit',
-          currency: 'eur',
-          owner: {
-            name: ev.target.name.value,
-            email: ev.target.email.value,
-          },
-          mandate: {
-            notification_method: 'email',
-          },
-        })
-        .then((payload) => console.log('[source]', payload));
-    } else {
-      console.log("Stripe.js hasn't loaded yet.");
-    }
-  };
-  render() {
-    return (
-      <form onSubmit={this.handleSubmit}>
-        <label>
-          Name
-          <input name="name" type="text" placeholder="Jane Doe" required />
-        </label>
-        <label>
-          Email
-          <input
-            name="email"
-            type="email"
-            placeholder="jane.doe@example.com"
-            required
-          />
-        </label>
-        <label>
-          IBAN
-          <IbanElement
-            supportedCountries={['SEPA']}
-            onBlur={handlers.handleBlur}
-            onChange={handlers.handleChange}
-            onFocus={handlers.handleFocus}
-            onReady={handlers.handleReady}
-            {...createOptions(this.props.fontSize)}
-          />
-        </label>
-        <button>Pay</button>
-      </form>
-    );
-  }
-}
-const IbanForm = injectStripe(_IbanForm);
 
 class Checkout extends React.Component {
   constructor() {
