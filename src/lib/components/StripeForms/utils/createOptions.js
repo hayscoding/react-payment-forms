47b136e8fb7820193f315be143bb49769d862603
Hays Stanford <stanfordhays@gmail.com>
2019-11-17 15:42:03 -0600

    added code for createOptions.js function & utilized in SplitForm.js

---
+++ src/lib/components/StripeForms/utils/createOptions.js
@@ -0,0 +1,21 @@
+const createOptions = (fontSize, padding) => {
+  return {
+    style: {
+      base: {
+        fontSize,
+        color: '#424770',
+        letterSpacing: '0.025em',
+        fontFamily: 'Source Code Pro, monospace',
+        '::placeholder': {
+          color: '#aab7c4',
+        },
+        padding,
+      },
+      invalid: {
+        color: '#9e2146',
+      },
+    },
+  };
+};
+
+export default createOptions;
