d19f7155fe36e25a08ffdc8e777d830c550518c0
Hays Stanford <stanfordhays@gmail.com>
2019-11-17 15:46:14 -0600

    removed commented SplitForm code from StripeForm.js

--- src/lib/components/StripeForms/StripeForm.js
+++ src/lib/components/StripeForms/StripeForm.js
@@ -75,57 +75,6 @@ export default class StripeForm extends Component {
   }
 }
 
-// class _SplitForm extends React.Component {
-//   handleSubmit = (ev) => {
-//     ev.preventDefault();
-//     if (this.props.stripe) {
-//       this.props.stripe
-//         .createToken()
-//         .then((payload) => console.log('[token]', payload));
-//     } else {
-//       console.log("Stripe.js hasn't loaded yet.");
-//     }
-//   };
-//   render() {
-//     return (
-//       <form onSubmit={this.handleSubmit}>
-//         <label>
-//           Card number
-//           <CardNumberElement
-//             onBlur={handlers.handleBlur}
-//             onChange={handlers.handleChange}
-//             onFocus={handlers.handleFocus}
-//             onReady={handlers.handleReady}
-//             {...createOptions(this.props.fontSize)}
-//           />
-//         </label>
-//         <label>
-//           Expiration date
-//           <CardExpiryElement
-//             onBlur={handlers.handleBlur}
-//             onChange={handlers.handleChange}
-//             onFocus={handlers.handleFocus}
-//             onReady={handlers.handleReady}
-//             {...createOptions(this.props.fontSize)}
-//           />
-//         </label>
-//         <label>
-//           CVC
-//           <CardCVCElement
-//             onBlur={handlers.handleBlur}
-//             onChange={handlers.handleChange}
-//             onFocus={handlers.handleFocus}
-//             onReady={handlers.handleReady}
-//             {...createOptions(this.props.fontSize)}
-//           />
-//         </label>
-//         <button>Pay</button>
-//       </form>
-//     );
-//   }
-// }
-// const SplitForm = injectStripe(_SplitForm);
-
 class _PaymentRequestForm extends React.Component {
   constructor(props) {
     super(props);
